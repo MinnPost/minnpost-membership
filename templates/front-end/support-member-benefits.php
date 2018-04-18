f97900b4bfbb5d7f7f69bec04752ce9c16953bea
Jonathan Stegall <jstegall@minnpost.com>
2018-04-18 09:56:38 -0500

    fix front end markup that depends on javascript

--- templates/front-end/support-member-benefits.php
+++ templates/front-end/support-member-benefits.php
@@ -25,33 +25,31 @@ get_header(); ?>
 										<article class="m-membership-member-level m-membership-member-level-<?php echo $record['slug']; ?>">
 											<section class="m-member-level-brief">
 												<h2><?php echo esc_html( $record['name'] ); ?></h2>
-												<div class="flipper">
-													<div class="amount">
-														<h3 data-one-time="<?php echo $ranges['yearly']; ?>" data-year="<?php echo $ranges['yearly']; ?>" data-month="<?php echo $ranges['monthly']; ?>">
-															<?php echo $ranges[ $this->member_levels->get_frequency_options( get_option( $this->option_prefix . 'default_frequency', '' ) )['id'] ]; ?>
-														</h3>
-														<?php $frequency_options = $this->member_levels->get_frequency_options(); ?>
-														<?php if ( ! empty( $frequency_options ) ) : ?>
-															<div class="m-form-item">
-																<select>
-																	<?php foreach ( $frequency_options as $option ) : ?>
-																		<?php
-																		if ( $this->member_levels->get_frequency_options( get_option( $this->option_prefix . 'default_frequency', '' ) )['value'] === $option['value'] ) {
-																			$selected = ' selected';
-																		} else {
-																			$selected = '';
-																		}
-																		?>
-																		<option value="<?php echo $option['value']; ?>"<?php echo $selected; ?>><?php echo $option['text']; ?></option>
-																	<?php endforeach; ?>
-																</select>
-															</div>
-														<?php endif; ?>
-														<a href="#" class="a-button a-button-choose">Set Amount</a>
-													</div>
-													<div class="enter">
-														what
-													</div>
+												<div class="amount">
+													<h3 data-one-time="<?php echo $ranges['yearly']; ?>" data-year="<?php echo $ranges['yearly']; ?>" data-month="<?php echo $ranges['monthly']; ?>">
+														<?php echo $ranges[ $this->member_levels->get_frequency_options( get_option( $this->option_prefix . 'default_frequency', '' ) )['id'] ]; ?>
+													</h3>
+													<?php $frequency_options = $this->member_levels->get_frequency_options(); ?>
+													<?php if ( ! empty( $frequency_options ) ) : ?>
+														<div class="m-form-item">
+															<select id="membership-frequency-preview-<?php echo $key + 1; ?>" name="membership-frequency-preview-<?php echo $key + 1; ?>" class="a-form-item-membership-frequency">
+																<?php foreach ( $frequency_options as $option ) : ?>
+																	<?php
+																	if ( $this->member_levels->get_frequency_options( get_option( $this->option_prefix . 'default_frequency', '' ) )['value'] === $option['value'] ) {
+																		$selected = ' selected';
+																	} else {
+																		$selected = '';
+																	}
+																	?>
+																	<option value="<?php echo $option['value']; ?>"<?php echo $selected; ?>><?php echo $option['text']; ?></option>
+																<?php endforeach; ?>
+															</select>
+														</div>
+													<?php endif; ?>
+													<a href="#" class="a-button a-button-choose"><?php echo __( 'Set Amount', 'minnpost-membership' ); ?></a>
+												</div>
+												<div class="enter">
+													<input class="a-button a-button-choose" name="membership-submit-<?php echo $key + 1; ?>" value="<?php echo __( 'Give Now', 'minnpost-membership' ); ?>" type="submit">
 												</div>
 											</section>
 										</article>
