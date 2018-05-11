5f146abc5b5a2009750d70fc2803967f39602db4
Jonathan Stegall <jstegall@minnpost.com>
2018-05-11 11:15:17 -0500

    it does still need `wpautop`

--- templates/blocked/single.php
+++ templates/blocked/single.php
@@ -17,7 +17,7 @@ $user_state = $minnpost_membership->user_info->get_user_access( '' )['state'];
 	<div id="primary" class="m-layout-primary">
 		<main id="main" class="site-main" role="main">
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'm-post', 'm-post-paywalled' ) ); ?>>
+			<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'm-post', 'm-post-blocked' ) ); ?>>
 
 				<header class="m-entry-header<?php if ( is_singular() ) { ?> m-entry-header-singular<?php } ?><?php if ( is_single() ) { ?> m-entry-header-single<?php } ?>">
 					<?php
@@ -30,7 +30,7 @@ $user_state = $minnpost_membership->user_info->get_user_access( '' )['state'];
 				</header><!-- .m-entry-header -->
 
 				<div class="m-entry-content">
-					<?php echo apply_filters( 'the_content', $minnpost_membership->front_end->get_option_based_on_user_status( $minnpost_membership->option_prefix . 'post_access_blocked_message', $user_state ) ); ?>
+					<?php echo wpautop( apply_filters( 'the_content', $minnpost_membership->front_end->get_option_based_on_user_status( $minnpost_membership->option_prefix . 'post_access_blocked_message', $user_state ) ) ); ?>
 				</div><!-- .m-entry-content -->
 
 			</article><!-- #post-## -->
