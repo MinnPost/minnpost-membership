fbfd708738bb30fd92a0367b4a60400affc78f84
Jonathan Stegall <jstegall@minnpost.com>
2018-06-11 13:59:05 -0500

    add blank templates for those benefit pages

---
+++ templates/front-end/account-benefits-fan-club.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying the fan club vote page
+ *
+ */
+get_header(); ?>
+
+<?php
+get_footer();
---
+++ templates/front-end/account-benefits-partner-offers.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying the partner offer claim page
+ *
+ */
+get_header(); ?>
+
+<?php
+get_footer();
