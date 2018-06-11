674d060ded35ce2efbc062619de5cb93c7e03b59
Jonathan Stegall <jstegall@minnpost.com>
2018-06-11 14:10:55 -0500

    the two pages share this much, so there it is

--- templates/front-end/account-benefits-fan-club.php
+++ templates/front-end/account-benefits-fan-club.php
@@ -4,6 +4,18 @@
  *
  */
 get_header(); ?>
+<?php
+global $minnpost_membership;
+$user_state = $minnpost_membership->user_info->get_user_access( '', 'account-benefits-fan-club' )['state'];
+?>
+
+	<div id="primary" class="m-layout-membership o-fan-club m-page">
+		<main id="main" class="site-main" role="main">
+			<header class="m-entry-header m-entry-header-singular">
+				<h1 class="a-entry-title"><?php echo get_option( $minnpost_membership->option_prefix . 'account-benefits-fan-club_title', '' ); ?></h1>
+			</header>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_footer();
