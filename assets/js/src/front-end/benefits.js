eaef0a145c4670305ae1f0e37fdca108f4a21a07
Jonathan Stegall <jstegall@minnpost.com>
2018-06-22 16:44:09 -0500

    basic ajax for claiming an offer. still needs some work, but this functions at least

--- assets/js/src/front-end/benefits.js
+++ assets/js/src/front-end/benefits.js
@@ -1,64 +1,55 @@
 ( function( $ ) {
 
-	function checkZipCountry(city_field, state_field, zip_field, country_field) {
-
-		var country = country_field.val();
-		if (country == '') {
-			country = 'US';
-			country_field.val(country);
-		}
-		var zip = zip_field.val();
-
-		if (zip !== '') {
-
-			var location = {
-				zip_code: zip,
-				country: country
-			}
-
-			jQuery.ajax({
-		        type: 'GET',
-		        url: user_account_management_rest.site_url + user_account_management_rest.rest_namespace + '/check-zip',
-		        data: location,
-		        dataType: 'json',
-		        success: function(response) {
-		        	if (response.status === 'success') {
-						var location = '';
-						location += response.city;
-						$(city_field).val(response.city);
-						if (response.city !== response.state) {
-							location += ', ' + response.state;
-							$(state_field).val(response.state);
-						}
-						if (country !== 'US') {
-							location += ', ' + country;
-						}
-						$('.location small').text(location);
-					} else {
-						$('.location small').text('');
-					}
-		        }
-		    });
-		}
-	}
-
-	function benefitButton() {
+	function benefitForm() {
 		$( '.a-benefit-button.a-button-disabled' ).removeAttr( 'disabled' );
 		$( '.a-benefit-button' ).click( function( event ) {
-			//$( '.m-benefit-message' ).hide();
-			$( '.m-benefit-message' ).removeClass()
-			var thisMessage = $( this ).parent().find( '.m-benefit-message' );
-			if ( $( this ).hasClass( 'a-button-disabled' ) ) {
+			event.preventDefault();
+			var $button  = $( this );
+			var settings = minnpost_membership_settings;
+			// reset the message for current status
+			$( '.m-benefit-message' ).removeClass( 'm-benefit-message-visible m-benefit-message-error m-benefit-message-info m-benefit-message-success' );
+			if ( $( $button ).hasClass( 'a-button-disabled' ) ) {
 				//thisMessage.html( thisMessage.data( 'message-all-claimed' ) );
 				//thisMessage.fadeIn( 'slow' );
-				event.preventDefault(); // this should go on enabled buttons too, but for now just here
+			} else {
+				// set button to processing
+				$button.text( 'Processing' ).addClass( 'a-button-disabled' );
+				// set ajax data
+				var data = {};
+				var benefitType = $( 'input[name="benefit-name"]' ).val();
+				if ( 'partner-offers' === benefitType ) {
+				    data = {
+				        'action' : 'benefit_form_submit',
+				        'minnpost_membership_benefit_form_nonce' : $button.data( 'benefit-nonce' ),
+				        'current_url' : $( 'input[name="current_url"]').val(),
+				        'benefit-name': $( 'input[name="benefit-name"]').val(),
+				        'instance_id' : $( 'input[name="instance-id-' + $button.val() + '"]' ).val(),
+				        'post_id' : $button.val(),
+				        'is_ajax' : '1',
+				    };
+				    $.post( settings.ajaxurl, data, function( response ) {
+					    if ( true === response.success ) {
+					    	console.log( 'woooo' );
+					    	$button.text( 'Claimed' ).removeClass( 'a-button-disabled' ).addClass( 'a-button-claimed' ).prop( 'disabled', true );
+					        // remove button and textarea
+					        //$button.remove();
+					        //$( '.report-a-bug-message' ).remove();
+
+					        // display success message
+					        //$( '.report-a-bug-response' ).html( response.data );
+					    } else {
+					    	$button.removeClass( 'a-button-disabled' );
+					    }
+
+					});
+			    }
 			}
 		});
 	}
 
 	$( document ).ready( function() {
-		if ( 0 < $( '.a-benefit-button' ).length ) {
-			benefitButton();
+		if ( 0 < $( '.m-form-membership-benefit' ).length ) {
+			benefitForm();
 		}
 	});
 
