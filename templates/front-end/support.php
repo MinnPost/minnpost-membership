b306c1990589b7d16760d4fff2e45a40c0b5eb43
Jonathan Stegall <jstegall@minnpost.com>
2019-08-01 12:01:49 -0500

    also make some urls out of that progress bar

--- templates/front-end/support.php
+++ templates/front-end/support.php
@@ -14,11 +14,25 @@ $user_id    = get_current_user_id();
 
 		<main id="main" class="site-main" role="main">
 
-			<div class="m-support-progress">
+			<div class="m-support-progress" aria-label="progress">
 				<ol>
-					<li class="panel--review"><span class="active">Choose</span></li>
-					<li class="panel--pay"><span>Payment</span></li>
-					<li class="panel--confirmation"><span>Thank You</span></li>
+					<?php $choose_url = site_url( '/support/' ); ?>
+					<?php if ( ! empty( $url_params ) ) : ?>
+							<?php $choose_url .= http_build_query( $url_params ); ?>
+					<?php endif; ?>
+					<li><a href="<?php echo $choose_url; ?>" class="active">Choose</a></li>
+					<?php
+					if ( ! empty( $url_params ) ) {
+						$pay_url  = defined( 'PAYMENT_PROCESSOR_URL' ) ? PAYMENT_PROCESSOR_URL : get_option( $this->option_prefix . 'payment_processor_url', '' );
+						$pay_url .= '&amp;' . http_build_query( $url_params );
+					}
+					?>
+					<?php if ( isset( $pay_url ) ) : ?>
+						<li><a href="<?php echo $pay_url; ?>"><?php echo __( 'Payment', 'minnpost-membership' ); ?></a></li>
+					<?php else : ?>
+						<li><span><?php echo __( 'Payment', 'minnpost-membership' ); ?></span></li>
+					<?php endif; ?>
+					<li><span><?php echo __( 'Thank You', 'minnpost-membership' ); ?></span></li>
 				</ol>
 			</div>
 
